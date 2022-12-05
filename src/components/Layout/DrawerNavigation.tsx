e3906823e7bdd197c52abd534295a4cf9dedee1a
ujLevi <levente.uj@viddl.hu>
2022-12-05 14:57:49 +0100

    mui, basic navigation added

---
+++ src/components/Layout/DrawerNavigation.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { NAV_ITEMS } from '../../constants';
+
+type DrawerNavigationProps = {
+  drawerOpen: boolean;
+  handleDrawerToggle: () => void;
+};
+
+export function DrawerNavigation({
+  drawerOpen,
+  handleDrawerToggle,
+}: DrawerNavigationProps) {
+  return (
+    <Box component="nav">
+      <Drawer
+        variant="temporary"
+        open={drawerOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{
+          keepMounted: true, // Better open performance on mobile.
+        }}
+        sx={{
+          display: { xs: 'block', sm: 'none' },
+          '& .MuiDrawer-paper': {
+            boxSizing: 'border-box',
+            width: 240,
+          },
+        }}
+      >
+        <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+          <Typography variant="h6" sx={{ my: 2 }}>
+            MUI
+          </Typography>
+          <Divider />
+          <List>
+            {NAV_ITEMS.map((item) => (
+              <ListItem key={item} disablePadding>
+                <ListItemButton sx={{ textAlign: 'center' }}>
+                  <ListItemText primary={item} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
+    </Box>
+  );
+}
