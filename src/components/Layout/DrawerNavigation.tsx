d7057d70bfee4b796d44ac29abfe86f5a07ea578
ujLevi <levente.uj@viddl.hu>
2022-12-05 15:41:24 +0100

    Layout moved to app.tsx

--- src/components/Layout/DrawerNavigation.tsx
+++ src/components/Layout/DrawerNavigation.tsx
@@ -1,17 +1,12 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import * as React from 'react';
 import { NAV_ITEMS } from '../../constants';
 
 type DrawerNavigationProps = {
