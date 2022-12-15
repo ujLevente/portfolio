50191d5f10c59bbb6a7b2ceee6a226a5297052f9
ujLevi <levente.uj@viddl.hu>
2022-12-15 18:04:20 +0100

    footer added

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,27 @@
+import { GitHub } from '@mui/icons-material';
+import { Box, Link } from '@mui/material';
+
 export function Footer() {
-    return null;
+    return (
+        <Box
+            sx={{
+                pt: 6,
+                pb: 3,
+            }}
+        >
+            <Link
+                href="https://github.com/ujLevente/portfolio"
+                variant="body2"
+                target="_blank"
+                sx={{
+                    alignItems: 'center',
+                    display: 'flex',
+                    justifyContent: 'center',
+                }}
+            >
+                <GitHub sx={{ fontSize: '19px', mr: 0.5 }} />
+                Designed & Built by Új Levente
+            </Link>
+        </Box>
+    );
 }
