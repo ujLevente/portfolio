433c98aafb4ec4086eba8cdd947c448767cdad6d
ujLevente <asdlevente@gmail.com>
2022-12-20 21:13:49 +0100

    contact added to menu

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
 import CodeIcon from '@mui/icons-material/Code';
 import EqualizerIcon from '@mui/icons-material/Equalizer';
+import MessageIcon from '@mui/icons-material/Message';
 import WorkIcon from '@mui/icons-material/Work';
 import { SvgIconTypeMap } from '@mui/material';
 import { OverridableComponent } from '@mui/material/OverridableComponent';
@@ -15,4 +16,5 @@ export const NAV_ITEMS: NavItem[] = [
     { name: 'Experience', sectionId: 'experience', Icon: WorkIcon },
     { name: 'Projects', sectionId: 'projects', Icon: CodeIcon },
     { name: 'Skillset', sectionId: 'skillset', Icon: EqualizerIcon },
+    { name: 'Contact', sectionId: 'contact', Icon: MessageIcon },
 ];
